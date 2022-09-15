e7c588cbf9d7fc0cc8ada4e9d5b6cfe30083605a
korhonj19 <juuso.o.korhonen@aalto.fi>
2022-09-15 16:32:39 +0300

    feature extractor and frame classes

--- src/slam.py
+++ src/slam.py
@@ -13,6 +13,27 @@ class FeatureExtractor:
         return kp, des
 
 
+class FeatureMatcher():
+    def __init__(self):
+        #self.matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        self.matcher = cv2.BFMatcher()
+    def match_features(self, frame_cur, frame_prev):
+        kp1, feat1 = frame_cur.keypoints, frame_cur.features
+        kp2, feat2 = frame_prev.keypoints, frame_prev.features
+        # Match descriptors.
+        matches = self.matcher.knnMatch(feat1,feat2,k=2)
+        # Apply ratio test
+        good = []
+        for m,n in matches:
+            if m.distance < 0.01*n.distance:
+                good.append([m])
+        # Sort them in the order of their distance.
+        #matches = sorted(matches, key = lambda x:x.distance)
+        return good
+
+
+
+
 
 class Frame:
     def __init__(self, rgb_fp, d_path, feature_extractor):
@@ -30,6 +51,7 @@ class Frame:
         
 
 if __name__=="__main__":
+    # Filepaths
     cur_dir = str(Path(os.path.dirname(os.path.realpath(__file__))).parents[1])
     dir_rgb = cur_dir + "/visual_slam/data/ICL_NUIM/rgb/"
     dir_depth = cur_dir + "/visual_slam/data/ICL_NUIM/depth/"
@@ -37,14 +59,33 @@ if __name__=="__main__":
     if is_WINDOWS:
         dir_rgb = dir_rgb.replace("/", "\\")
         dir_depth = dir_depth.replace("/", "\\")
-
+    # Initialize
     feature_extractor = FeatureExtractor()
+    feature_matcher = FeatureMatcher()
+    fp_rgb = dir_rgb + str(1) + ".png"
+    fp_depth = dir_depth + str(1) + ".png"
+    frame = Frame(fp_rgb, fp_depth, feature_extractor)
+    kp, features, rgb = frame.process_frame() 
+    # Display
+    img2 = cv2.drawKeypoints(rgb, kp, None, color=(0,255,0), flags=0)
+    cv2.imshow('a', img2)
+    cv2.waitKey(0)
+    #
+    prev_frame = frame
 
-    for i in range(1,100):
+    for i in range(2,100):
         fp_rgb = dir_rgb + str(i) + ".png"
         fp_depth = dir_depth + str(i) + ".png"
-        frame = Frame(fp_rgb, fp_depth, feature_extractor)
-        kp, features, rgb = frame.process_frame()
-        img2 = cv2.drawKeypoints(rgb, kp, None, color=(0,255,0), flags=0)
-        cv2.imshow('a', img2)
+        # Feature Extraction for current frame
+        cur_frame = Frame(fp_rgb, fp_depth, feature_extractor)
+        kp, features, rgb = cur_frame.process_frame()
+        # Feature Matching to previous frame
+        matches = feature_matcher.match_features(cur_frame, prev_frame)    
+        # Display
+        #img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        img3 = cv2.drawMatchesKnn(prev_frame.rgb,prev_frame.keypoints,cur_frame.rgb,cur_frame.keypoints,matches,None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        #img2 = cv2.drawKeypoints(rgb, kp, None, color=(0,255,0), flags=0)
+        cv2.imshow('a', img3)
         cv2.waitKey(0)
+        #
+        prev_frame = cur_frame
